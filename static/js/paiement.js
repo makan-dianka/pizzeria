d606b2d556b5a4a9fa1ecb7d1c1532a2a6f07b73
Makan DIANKA <makan.dianka@hotmail.com>
2023-05-24 16:12:06 +0200

    static files

---
+++ static/js/paiement.js
@@ -0,0 +1,57 @@
+// style css 
+var style = {
+    base : {
+        color: '#32325d',
+        fontFamily: "helvetica sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "15px",
+        '::placeholder': {
+            color: "#aab7c4",
+        }
+    },
+    invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a",
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', async () => {
+    const stripe = Stripe('pk_test_51JRFBQKoo0ZeB3KHwFkaqb6UrxYQVjW2IEpvD4MVCC5s69hbGNAs6iCaEu3RkKtQjPTRquA3yx4HN2t6IHfTY8Fq008Vkaur6D')
+    const elements = stripe.elements()
+    var card = elements.create("card", {style: style});
+    card.mount("#card-element");
+  
+    card.addEventListener('change', function(event){
+      var displayError=document.getElementById("card-errors")
+      if (event.errors) {
+          displayError.textContent = event.error.message
+      }else{
+          displayError.textContent = ''
+      }
+    })
+  
+    // create stripe token 
+    var form = document.getElementById("payment-form")
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        stripe.createToken(card).then(function(result){
+            if (result.error) {
+                var errorElement = document.getElementById("card-errors")
+                errorElement.textContent = result.error.message
+            }else{
+                stripeTokenHandler(result.token)
+            }
+        })
+    })
+  
+    // append stripe token to the form 
+    function stripeTokenHandler(token){
+      var form = document.getElementById("payment-form")
+      var hiddenInput = document.getElementById("input")
+      hiddenInput.setAttribute('name', 'stripeToken')
+      hiddenInput.setAttribute('value', token.id)
+      form.appendChild(hiddenInput)
+      form.submit()
+    }
+  
+  })
